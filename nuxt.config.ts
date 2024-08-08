55e5ad46e915525aec0bc30c9636a8a57ce6356e
OlivierC <coq.olivier36@gmail.com>
2024-08-08 01:02:24 -0400

    N Love - SHOSH, Kelsey

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  ssr: true,
+  devtools: { enabled: true },
+  css: ["~/assets/style/main.scss"],
+  modules: [],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
