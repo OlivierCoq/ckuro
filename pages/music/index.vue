ca221cb8d0af2d1d8142e914bda87fda8fc49cc0
OlivierC <coq.olivier36@gmail.com>
2024-08-22 00:21:46 -0400

    Adjusting music and inner page template - Hiding, by Ben Bohmer and Lykke Li

--- pages/music/index.vue
+++ pages/music/index.vue
@@ -14,13 +14,41 @@
         </div>
       </div>
       <TemplateNav :links="state.nav_links" />
+      <!-- Artists here -->
     </div>
   </div>
+  <!-- Music player: -->
+  <div></div>
 </template>
 <script setup>
+// config/setup
+const config = useRuntimeConfig();
+import qs from "qs";
 definePageMeta({
   title: "cKuro - Music",
   descripton: "Captain Kuro's music n shit",
-  layout: "musi",
+  layout: "music",
+});
+const state = reactive({
+  nav_links: [],
+});
+
+fetch(
+  `${config.public.NUXT_STRAPI_URL}/api/site-nav?${qs.stringify(
+    {
+      populate: ["links", "links.imagery"],
+    },
+    { arrayFormat: "comma" },
+  )}`,
+  {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  },
+).then(async (data) => {
+  const res = await data.json();
+  // console.log("fuuuuuuuuck", res.data);
+  state.nav_links = res.data.links;
 });
 </script>
